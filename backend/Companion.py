00efe007f21d68623214137576d875e953b31ed5
a-s-o-m <asoteromontanez@miners.utep.edu>
2022-03-02 16:43:34 -0700

    modified Companion.py

--- backend/Companion.py
+++ backend/Companion.py
@@ -1,13 +1,43 @@
 class Companion:
-    def __init__(self, description, age, height, extraversion, dog_person, gender):
+    def __init__(self, description, age, height, personality, pet_preference, gender):
+        str_parameters = [description, personality, pet_preference, gender]
+        
+        for parameter in str_parameters:
+            if type(parameter) != str: raise TypeError(f"{parameter} must be a string.")
+
+        if type(age) != int:
+            raise TypeError("Age must be an integer")
+        
+        if type(height) not in [int, float]:
+            raise TypeError("Height must be a number")
+        
         self.description = description
         self.age = age
         self.height = height
-        self.extraversion = extraversion
-        self.dog_person = dog_person
+        self.personality = personality
+        self.pet_preference = pet_preference
         self.gender = gender
 
-    def calculate_match(pref_age, preg_height, pref_personality, pref_pet, pref_gender):
+    def calculate_match(pref_age, pref_height, pref_personality, pref_pet, pref_gender):
         """Returns match percentage"""
-        total = 0
+        pref_matches = 0
+
+        if pref_age[0] <= self.age <= pref_age[1]:
+            pref_matches += 1
+
+        if pref_height[0] <= self.height <= pref_height[1]:
+            pref_matches += 1
+
+        if pref_personality == self.pref_personality:
+            pref_matches += 1
+
+        if pref_pet == self.pref_pet:
+            pref_matches += 1
+
+        if pref_gender == self.pref_gender:
+            pref_matches += 1
+
+        return pref_matches / (0.5)
+
+
         
