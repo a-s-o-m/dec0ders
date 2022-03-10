6203fc7a99bda870124ac44b3b0629c20851d37e
Renee Catanach <rcat@techexchange.in>
2022-03-10 01:21:31 +0000

    Updated test cases for the Companion file, and added comments for easy read

--- backend/companionCatalog.py
+++ backend/companionCatalog.py
@@ -4,8 +4,9 @@ from Companion import Companion
 class companionCatalog:
     def __init__(self):
         '''
-        data strucuture that stores all companions and their relevant information. Each companion should have
-        a name, description of personality, pet preference, and sex
+        data strucuture that stores all companions and their relevant information.
+        All companions listed below are ones we have initially had on our website for project 1
+        Each companion should have a name, description of personality, pet preference, and sex
         '''
         self.companions = [Companion('Dylan OBrien', 25, 200, 'extrovert','dog', 'male'),
         Companion('Amy Wind', 24, 110, 'introvert', 'cat', 'female'),
@@ -14,14 +15,25 @@ class companionCatalog:
         Companion('Cristiano Lopez', 32, 200, 'introvert', 'dog', 'male'),
         Companion('Helena Rios', 27, 175, 'introvert', 'dog', 'female'),
         Companion('Kim Tae-hyung', 26, 200, 'introvert', 'dog', 'male'),
-        Companion('Stephanie Pierce', 19, 120, 'extrovert', 'cat', 'female')]
+        Companion('Stephanie Pierce', 19, 120, 'extrovert', 'cat', 'female')] 
 
+        
 
-    def addCompanion(self, new_companion):
+    def add_companion(self, new_companion):
         '''
-        The addCompanion function enables users to be able to add a new companion to the catalog if they wish. 
+        The add_companion function enables users to be able to add a new companion to the catalog if they wish. 
         This functions gathers all information and then appends the newly created companion to the catalog.
         '''
         if (not isinstance(new_companion, Companion)):
             raise TypeError("new companion must be instance of Companion class")
         self.companions.append(new_companion) # add new companion to catalog 
+
+    def remove_companion(self, del_companion):
+        '''
+        The remove_companion function enables companions to be taken off of the catalog. Their attributes and self within 
+        the catalog will be deleted, and updated.
+        '''
+        if (not isinstance(del_companion, Companion)):
+            raise TypeError("the companion in which you wish to delete must be instance of Companion class")
+        if (self.companions.__contains__(del_companion)):
+            self.companions.remove(del_companion) 
--- backend/test_companion.py
+++ backend/test_companion.py
@@ -2,49 +2,57 @@ import unittest
 
 from Companion import Companion
 
-# data for tests
+# data for test_init_types and for test_init_values
 description = ''
 age = 18
-height = "6'1"
+height = 180
 personality = 'introvert'
 pet_preference = 'dog'
 gender = 'male'
+name = 'Dylan Obrien'
+companion = Companion(name, age, height, personality, pet_preference, gender)
 
-companion = Companion(description, age, height, personality, pet_preference, gender)
+# data for match test
+pref_age = [20,25]
+pref_height = [180,200]
+pref_personality = 'introvert'
+pet_preference = 'dog'
+sex_preference = 'male'
 
-# python3 -m unittest test_companion
+
 class TestCompanion(unittest.TestCase):
 
     def test_init_types(self):
         # description, personality, pet_preference, gender must be strings
-        self.assertRaises(TypeError, 'Billy Stewart', 18, 150, False, 'cat', 'male') # personality trait must be string
-        self.assertRaises(TypeError, 'Timia Northcutt', 27, 'old', 'introvert', 1200, 'female') # animal preference must be string. age must be an int
-        self.assertRaises(TypeError, 'Kousei Richeson', 27, '25', 'extrovert', 'dog', 'male') # height must be int or float
-        self.assertRaises(TypeError, 'Vivian Alcala', '21', 175, 'introvert', 'dog', 'female') # age must be int
-        self.assertRaises(TypeError, 'Luis Montes', 27, 175, 'introvert', 89, 'female') # animal preference must be string
-        self.assertRaises(TypeError,'Renee Catanach', 27, 175, 'introvert', 'dog', False) # sex must be a string 
+        self.assertRaises(TypeError, Companion, 'Billy Stewart', 18, 150, False, 'cat', 'male') # personality trait must be string
+        self.assertRaises(TypeError, Companion, 'Timia Northcutt', 27, 'old', 'introvert', 1200, 'female') # animal preference must be string. age must be an int
+        self.assertRaises(TypeError, Companion, 'Kousei Richeson', 27, '25', 'extrovert', 'dog', 'male') # height must be int or float
+        self.assertRaises(TypeError, Companion, 'Vivian Alcala', '21', 175, 'introvert', 'dog', 'female') # age must be int
+        self.assertRaises(TypeError, Companion, 'Luis Montes', 27, 175, 'introvert', 89, 'female') # animal preference must be string
+        self.assertRaises(TypeError, Companion, 'Renee Catanach', 27, 175, 'introvert', 'dog', False) # sex must be a string 
        
 
     def test_init_values(self):
         # age must be >= 18
-        self.assertRaises(ValueError, 'Helena Rios', -52, 175, 'introvert', 'dog', 'female')
-        # age cannnot be negative
-        self.assertRaises(ValueError, 'Helena Rios', 101, 175, 'introvert', 'dog', 'female')
+        self.assertRaises(ValueError, Companion,'Helena Rios', -52, 175, 'introvert', 'dog', 'female')
+        # age cannnot be negative 
+        self.assertRaises(ValueError, Companion, 'Helena Rios', 101, 175, 'introvert', 'dog', 'female')
         # age must range from 18-100
-        self.assertRaises(ValueError, 'Helena RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR', 101, 175, 'introvert', 'dog', 'female')
+        self.assertRaises(ValueError, Companion,'Helena RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR', 101, 175, 'introvert', 'dog', 'female')
         # name should be between 2 and 30 characters long
-        self.assertRaises(ValueError, 'Helena Rios', 101, 1, 'introvert', 'dog', 'female')
+        self.assertRaises(ValueError, Companion, 'Helena Rios', 101, 1, 'introvert', 'dog', 'female')
         # Minimum height should be higher than 100cm, maximum height should be lower or equal to 270cm
-        self.assertRaises(ValueError, 'Helena Rios', 101, 1, 'fun', 'dog', 'female')
+        self.assertRaises(ValueError, Companion, 'Helena Rios', 101, 1, 'fun', 'dog', 'female')
         # Personality should be either "introvert" or "extrovert"
-        self.assertRaises(ValueError, 'Helena Rios', 101, 1, 'introvert', 'fish', 'female')
+        self.assertRaises(ValueError, Companion,'Helena Rios', 101, 1, 'introvert', 'fish', 'female')
         # Pet preference should be either "dog" or "cat" (Case-insensitive)
-        self.assertRaises(ValueError, 'Helena Rios', 101, 1, 'introvert', 'fish', 'dog')
+        self.assertRaises(ValueError, Companion,'Helena Rios', 101, 1, 'introvert', 'fish', 'dog')
         # Sex should be either "male" or "female" (Case-insensitive)
 
     def test_match(self):
-        self.assertAlmostEqual(companion.calculate_match([20,25], [180,200], 'introvert', 'dog', 'male'),5555) 
-        # TODO: ask angel about match function
+        self.assertAlmostEqual(companion.calculate_match(pref_age, pref_height, pref_personality, pet_preference, sex_preference),80) 
+        
 
 # if __name__ == "__main__":
 #     unittest.main() 
+# python3 -m unittest test_companion --> command line 
