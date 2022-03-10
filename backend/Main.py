815c9648ea343f2a8958a0993416e49748cdb708
Samantha Roberts <Samantha@techexchange.in>
2022-03-10 03:32:30 +0000

    test

---
+++ backend/Main.py
@@ -0,0 +1,12 @@
+from Companion import Companion
+from user import User
+from companionCatalog import companionCatalog
+
+class Main:
+    '''def __init__(self):
+        # create new user
+        self.user = User("name: str, age: int, height: float, personality: str")
+        # take shelfName input
+        print(f"Welcome, {self.user.username}!")
+        '''
+Companion("sam", "5", "180", "bright", "dog", "female")
