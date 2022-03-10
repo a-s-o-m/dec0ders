ac7447724f53c45e82e7607a02a5b732ded01e94
Victor Akpan <victor@techexchange.in>
2022-03-10 01:12:56 +0000

    completed test cases

--- backend/test_companion_catalog.py
+++ backend/test_companion_catalog.py
@@ -3,11 +3,11 @@ from Companion import Companion
 from companionCatalog import CompanionCatalog
 from user import User
 
-# python3 -m unittest test_companion
+# python3 -m unittest test_companion_catalog.py
 class TestCompanion(unittest.TestCase):
 
     def test_add_companion(self):
-        user = User('Johnny', 'Test', 'jtest', 'jt@mail.com', '0123456789', 'test')
+        user = User('Johnny', 'Test', 'jtest', 'jt@mail.com', '0123456789', '12345')
         # shoudln't be able to add object that isnt a companion to companions list.
         self.assertRaises(TypeError, CompanionCatalog.addCompanion, user)
         self.assertRaises(TypeError, CompanionCatalog.addCompanion, '')
@@ -15,10 +15,8 @@ class TestCompanion(unittest.TestCase):
         # transform user to companion
         catalog = CompanionCatalog()
         catalogSize = len(catalog.companions)
-        user.become_companion(Companion(user.fullname, 65, 150, 'chill', 'cat', 'male'))
+        user.become_companion(Companion(user.fullname, 65, 150, 'introvert', 'cat', 'male'))
         catalog.addCompanion(user.companionAccount)
         # check if companions list is sucessfully updated when a companion is added.
         self.assertEqual(catalogSize+1, len(catalog.companions))
     
-# if __name__ == "__main__":
-#     unittest.main()
