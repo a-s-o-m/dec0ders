a312b7a7a92347f195d65e86745ca0847ab987ea
Samantha Roberts <Samantha@techexchange.in>
2022-03-10 07:25:04 +0000

    test

--- backend/Main.py
+++ backend/Main.py
@@ -1,12 +1,44 @@
 from Companion import Companion
 from user import User
-from companionCatalog import companionCatalog
+from companionCatalog import CompanionCatalog
 
 class Main:
-    '''def __init__(self):
-        # create new user
-        self.user = User("name: str, age: int, height: float, personality: str")
-        # take shelfName input 
-        print(f"Welcome, {self.user.username}!")
-        ''
-Companion("sam", "5", "180", "bright", "dog", "female")  
+    def __init__(self):
+        
+        #Ask user if they want to become a user
+        newUSerQuestion = input("Do you want to become a new user (Y/y/Yes/yes)")
+        if newUSerQuestion == "y" or newUSerQuestion == "Y" or newUSerQuestion == "yes" or newUSerQuestion == "Yes":
+            self.user = self.createNewUser()
+            print(f"Welcome, {self.user.username}!")
+        else:
+            self.user = None
+            print("User is not created")
+
+
+    def createNewUser(self):
+        firstName = input("Enter your first name")
+        lastName = input("Enter your last name")
+        userName = input("Create a username")
+        email = input("Enter your email")
+        phoneNumber = input("Enter your phone number")
+        password = input("Create a password")
+        newUser = User(firstName, lastName, userName, email, phoneNumber, password)
+        return newUser
+
+    def makeUserCompanion(self, user):
+        age = input("Enter your age")
+        height = input("Enter your height")       
+        personality = input("Enter your personality type")
+        pet_preference = input("Enter your pet preference")
+        sex = input("Enter your sex")
+
+        #Create a new companion
+        companion = Companion(user.name, age, height, personality, pet_preference, sex)
+
+        #Add new companion to the companion catalog
+        catalog = CompanionCatalog()
+        catalog.addCompanion(companion)
+
+        return companion
+
+    
