58129f3a7a3e9bb48e94001f0718801ffd0fc683
Renee Catanach <rcat@techexchange.in>
2022-04-03 23:45:00 +0000

    Added css to browsing page and was able to route to the pictures

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask_pymongo import PyMongo
 from flask import Flask, render_template, request, redirect, session, url_for
-<<<<<<< HEAD
 from seed_library import seed_companions
-=======
-from backend.user import User
->>>>>>> 1c1140a97533711a32d8cfc8e8dc1a02bbc58c4f
+#from backend.user import User
 import secrets
 import os
 
@@ -51,7 +48,7 @@ def signup():
             email = request.form['email']
             phone_number = request.form['phone_number']
             password = request.form['password']
-            user = User(firstname, lastname, username, email, phone_number, password)
+            #user = User(firstname, lastname, username, email, phone_number, password)
             #encode password for hashing
             password = request.form['password'].encode("utf-8")
             # create new user
@@ -100,6 +97,13 @@ def login():
 def browsing():
     #collection = mongo.db.library
     return render_template('browsing.html', companions = companions) 
+
+#static route
+@app.route('/<path:path>')
+def get_dir(path):
+    #collection = mongo.db.library
+    #print("Hey now, your ", path) 
+    return render_template(path) 
 
 
 
--- seed_library.py
+++ seed_library.py
@@ -1,10 +1,11 @@
 seed_companions = [
-        {"name":'Dylan OBrien', "age":25, "height":200, "type":'extrovert',"animal":'dog', "gender":'male', "pic": "{{ url_for('static', filename='/assets/man1.jpg')}}"},
-        {"name":'Amy Wind',"age": 24, "height":110,  "type":'introvert',"animal": 'cat', "gender":'female',"pic": "{{ url_for('static', filename='/assets/women1.jpg')}}"},
-        {"name":'Steve Johnson', "age":35, "height": 250, "type":'extrovert',"animal":'cat',"gender":'male',"pic": "{{ url_for('static', filename='/assets/man2.jpg')}}"},
-        {"name":'Stephanie Cheng', "age":30, "height":101,"type": 'extrovert', "animal":'cat', "gender":'female',"pic": "{{ url_for('static', filename='/assets/women2.jpg')}}"},
-        {"name":'Cristiano Lopez', "age":32, "height":200, "type":'introvert', "animal":'dog', "gender":'male',"pic": "{{ url_for('static', filename='/assets/men3.jpg')}}"},
-        {"name":'Helena Rios', "age": 27, "height": 175,"type": 'introvert', "animal": 'dog', "gender":'female',"pic": "{{ url_for('static', filename='/assets/women3.jpg')}}"},
-        {"name":'Kim Tae-hyung', "age":26,"height": 200, "type":'introvert', "animal":'dog', "gender":'male',"pic": "{{ url_for('static', filename='/assets/man4.jpg')}}"},
-        {"name":'Stephanie Pierce', "age":19, "height":120,"type": 'extrovert', "animal":'cat', "gender":'female',"pic": "{{ url_for('static', filename='/assets/women4.jpg')}}"}
+        {"name":'Dylan OBrien', "age":25, "height":200, "type":'extrovert',"animal":'dog', "gender":'male', "pic": "static/assets/man1.jpg"},
+        {"name":'Amy Wind',"age": 24, "height":110,  "type":'introvert',"animal": 'cat', "gender":'female',"pic": "static/assets/woman1.jpg"},
+        {"name":'Steve Johnson', "age":35, "height": 250, "type":'extrovert',"animal":'cat',"gender":'male',"pic": "static/assets/man2.jpg"},
+        {"name":'Stephanie Cheng', "age":30, "height":101,"type": 'extrovert', "animal":'cat', "gender":'female',"pic": "static/assets/woman2.jpg"},
+        {"name":'Cristiano Lopez', "age":32, "height":200, "type":'introvert', "animal":'dog', "gender":'male',"pic": "static/assets/men3.jpg"},
+        {"name":'Helena Rios', "age": 27, "height": 175,"type": 'introvert', "animal": 'dog', "gender":'female',"pic": "static/assets/woman3.jpg"},
+        {"name":'Kim Tae-hyung', "age":26,"height": 200, "type":'introvert', "animal":'dog', "gender":'male',"pic": "static/assets/man4.jpg"},
+        {"name":'Stephanie Pierce', "age":19, "height":120,"type": 'extrovert', "animal":'cat', "gender":'female',"pic": "static/assets/woman4.jpg"},
+        {"name":'Shyla Vihaan', "age":30, "height":205,"type": 'extrovert', "animal":'cat', "gender":'male',"pic": "static/assets/men5.jpg"}
 ]
